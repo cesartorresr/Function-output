73c69ebc8737dfe81ee301efb3bf0a8db19eb319
CesarTorres4 <>
2021-06-02 22:21:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+  formated_name = f_name.title()
+  formated_lname = l_name.title()
+  return f"{formated_name} {formated_lname}"
+
+print(format_name("carlos", "hernaaNAdez"))
